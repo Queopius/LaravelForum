25c2d09edd8288de91cba8ceb9523d4387e754ed
Queopius <dev.queopius@gmail.com>
2020-11-20 18:34:03 +0100

    Update Database

--- config/database.php
+++ config/database.php
@@ -137,7 +137,7 @@ return [
         // ],
 
         'default' => [
-            'scheme' => 'tls',
+            //'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
