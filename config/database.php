6db8e0fc24ed4afb42e088370153cc958a6febdb
Queopius <dev.queopius@gmail.com>
2020-11-20 17:47:19 +0100

    quit schema

--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
         ],
 
         'default' => [
-            'scheme' => 'tls',
+            // 'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
