55804f08fc78702abcd86924a340177b8b02a987
Queopius A. S <dev.queopius@gmail.com>
2024-04-18 18:20:50 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,6 +34,20 @@ class UserFactory extends Factory
     }
 
     /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
      * Indicate that the model's name should be administrator.
      *
      * @return \Illuminate\Database\Eloquent\Factories\Factory
